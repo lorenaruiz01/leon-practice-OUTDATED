67b4f91ad9a1433e44cfbf7e7f1424de99c0e2c2
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-25 14:29:28 -0800

    add exercises

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// create a variable and assign it a number
+
+//minus 10 from that number
+
+//print that number to the console
+
+
+
+//create a variable that holds a value from the input
+
+//add 25 to that number
+
+//alert that number
