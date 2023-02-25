c6dbc1d2d478b4b896c933534032be547d76c4a5
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-25 14:33:44 -0800

    complete exercises for easy section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
+//EASY
 // create a variable and assign it a number
+    let num = 1
 
 //minus 10 from that number
+    num = num - 10
 
 //print that number to the console
+    console.log (num)
+        //log(num): here, log is a function and num is an argument, meaning you can pass a variable into a function as an argument
 
+//MEDIUM
+        //create a variable that holds a value from the input
 
-
-//create a variable that holds a value from the input
 
 //add 25 to that number
 
